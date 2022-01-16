fce6139bd8708bbe71c234ccb12899c57c119c7d
mrnogues <mrnogues89@gmail.com>
2022-01-16 23:29:22 +0100

    test: ci test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Hello world again II !
+            Hello world again!
         </p>
         <a
           className="App-link"
